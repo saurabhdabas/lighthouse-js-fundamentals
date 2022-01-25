1c07712079ace9c36d6cac799434a657e7f02d6d
Saurabh Dabas <developerdabas@gmail.com>
2022-01-25 06:16:06 +0000

    Exploring while loops

---
+++ chorus-while.js
@@ -0,0 +1,15 @@
+const chorus="We'll dance till night"
+let repeat=0;
+// while(repeat < 10){
+//   console.log(chorus)
+//   repeat++;
+// }
+
+/* Changing the key when repeat is 6*/
+while(repeat < 10){
+  console.log(chorus)
+  if (repeat === 6){
+    console.log("change the key ");
+  }
+  repeat++;
+}
