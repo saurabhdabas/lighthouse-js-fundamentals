a21ff253a623d9493fe8af5a1505ca722d68cb70
Saurabh Dabas <developerdabas@gmail.com>
2022-01-25 08:29:54 +0000

    loop-exercise

---
+++ loop.js
@@ -0,0 +1,16 @@
+let num=100;
+while(num < 201){
+  if( num % 3 === 0 && num % 4 === 0){
+    console.log("LoopyLighthouse");
+  }
+  else if(num % 3 === 0){
+    console.log("Loopy");
+  }
+  else if( num % 4 === 0){
+    console.log("Lighthouse");
+  }
+  else {
+    console.log(num);
+  }
+  num++ ;
+}
