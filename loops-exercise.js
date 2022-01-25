4cc02be9d46f9c0b0d07d90e5fa78a5dd72c3d59
Saurabh Dabas <developerdabas@gmail.com>
2022-01-25 07:09:46 +0000

    conditions rearranged

--- loops-exercise.js
+++ loops-exercise.js
@@ -1,14 +1,15 @@
 let num=100;
 while(num < 201){
-  if(num % 3 === 0)
-  console.log("Loopy")
+  if( num % 3 === 0 && num % 4 === 0){
+    console.log("LoopyLighthouse")
+  }
+  else if(num % 3 === 0){
+    console.log("Loopy")
+  }
   else if( num % 4 === 0){
     console.log("Lighthouse")
   }
-  else if( num % 3 === 0 && num % 4 === 0){
-    console.log("Loopy-Lighthouse")
-  }
-  else{
+  else {
     console.log(num);
   }
   num++ ;
